afb3d14d26e4664cf048d1170b6256f885ddf1d6
EndlessPixel <system-mini@outlook.com>
2026-09-11 20:43:39 +0800

    chore(command): 移除未使用的 OptionalArgument 导入

--- fakeplayer-core/src/main/java/io/github/hello09x/fakeplayer/core/command/CommandRegistry.java
+++ fakeplayer-core/src/main/java/io/github/hello09x/fakeplayer/core/command/CommandRegistry.java
@@ -5,6 +5,7 @@ import com.google.inject.Singleton;
 import dev.jorel.commandapi.CommandPermission;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.arguments.GreedyStringArgument;
+import dev.jorel.commandapi.arguments.OptionalArgument;
 import io.github.hello09x.devtools.command.HelpCommand;
 import io.github.hello09x.devtools.core.utils.ComponentUtils;
 import io.github.hello09x.fakeplayer.api.spi.ActionSetting;
